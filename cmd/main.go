d5489dc1fed0e10b74559c7339f9c650024971c6
Jesstin Swadley <contact@jesstin.com>
2025-11-09 23:44:26 -0800

    player & enemy take turns attacking each other

--- cmd/main.go
+++ cmd/main.go
@@ -22,19 +22,37 @@ func main() {
 
 	fmt.Println("Your opponent is " + enm.Name + " and its health is " + strconv.Itoa(int(enm.Health)))
 
-	attack := p.Attack(scanner)
+	// Use for loop to continuously perform actions
+	turns := 0
 
-	enm.Health = enm.Health - int32(attack)
+	for turns != 1 {
+		turns = Turn(p, enm, scanner)
+	}
+}
 
-	if enm.Health > 0 {
-		fmt.Println(enm.Name + " has survived your attack its health is " + strconv.Itoa(int(enm.Health)))
+func Turn(p *internal.Player, enm *internal.Enemy, scanner *bufio.Scanner) int {
+	// Need to add a system
+	if p.Health > 0 && enm.Health > 0 {
+		attack := p.Attack(scanner)
+
+		enm.Health = enm.Health - int32(attack)
+
+		fmt.Println(enm.Name + " current health is " + strconv.Itoa(int(enm.Health)))
+
+		enmAttack := enm.Attack()
+
+		p.Health = p.Health - int32(enmAttack)
+
+		fmt.Println(enm.Name + " has attacked you, your health is now " + strconv.Itoa(int(p.Health)))
+
+		return 0
+	} else if p.Health <= 0 {
+		fmt.Println("You have been defeated")
+
+		return 1
 	} else {
 		fmt.Println("You have defeated " + enm.Name)
+
+		return 1
 	}
-
-	enmAttack := enm.Attack()
-
-	p.Health = p.Health - int32(enmAttack)
-
-	fmt.Println(enm.Name + " has attacked you, your health is now " + strconv.Itoa(int(p.Health)))
 }
